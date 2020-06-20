6e90cf855a0eee4c707ead9168fb738ff94c635b
dkf-lab <pxlbuisness@gmail.com>
2020-06-20 18:13:20 +0930

    Added comments, fixed message looping and added space between new sent.

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import random
 import time
+import os
+#function for clearing console
+def clearLines():
+    for _  in range(3):
+        print('')
+clearScreen = lambda: os.system('cls')
+#define various varibles/lists we will be using later.
+prompteduser = 0
 sentence = []
 score = 0
 wpm = 0
 averagewpm = 0
 round = 1
+
+#Selecting a sentence list
+
 while True:
-    print("Type 'external' to use sentences.dat or press enter to exit.")
+    if prompteduser == 0:
+        print("Type 'external' to use sentences.dat or press enter to exit.")
+        prompteduser=1
     sent = input("Enter sentence:")
     if sent == "":
         break
     if sent == 'external':
         text_file = open("sentences.dat")
-        lines = text_file.read().splitlines()
-        #print(lines)
-        #print(len(lines))
+        lines = text_file.read().splitlines() #or use split(seperator)
         sentence = lines
         text_file.close()
         break
     sentence.append(sent)
+
+#The game itself
+
 while True:
     lensent = len(sentence) - 1 #figure out how long the list is, and then adjusting for line 5
     x = (random.randint(0, lensent)) #random number, determines what sentence comes up
@@ -27,6 +41,8 @@ while True:
     sentlength = len(sentence[x]) #defines length of sentence
     usrinput = input() #get user input
     if usrinput == 'exit':
+        clearScreen()
+        clearLines()
         print('Exiting! Your final score was',score,'and your average WPM was',int((averagewpm/round)))
         break
     else:
@@ -36,10 +52,16 @@ while True:
             score += 1
             round += 1
             averagewpm += wpm
+            print('')
             print("Correct! Your WPM was",int(wpm),"and your score is currently",score)
+            clearLines()
         else:
             if score == 0:
+                print('')
                 print("Incorrect! Your score is now",score)
+                clearLines()
             else:
                 score -=1
+                print('')
                 print("Incorrect! Your score is now",score)
+                clearLines()
